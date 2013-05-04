51dd6dc1645cb4d52cd6b08cd73799080d3f4099
Camilo Palma Pradena <cpalmapradena@gmail.com>
2013-05-03 21:53:08 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EventsvisApi::Application.config.secret_token = 'd885b56d0b7db3b2f1156cc5255bff54d8c3c257a4015c7cbfd018e3cb28318722d215c11a008d926c08d990e6cb4da25c3a0a42898c7d298986b25cfecd1b10'
